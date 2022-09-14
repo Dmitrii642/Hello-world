a89c04bcbda32edcff6c1382bdacfbf48c945173
Hello world <Hello world>
2022-09-14 11:35:22 +0300

    Initial Commit

---
+++ Hello world/Hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_worldApp.swift
+//  Hello world
+//
+//  Created by Дмитрий Яковлев on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
